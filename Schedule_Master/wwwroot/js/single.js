8912f0c0fbf56cfa90b9282b55c9af391864c1e5
burgonyapure <deejay.spory@gmail.com>
2020-05-12 10:40:22 +0200

    get users

--- Schedule_Master/wwwroot/js/single.js
+++ Schedule_Master/wwwroot/js/single.js
@@ -1,9 +1,33 @@
-﻿function regListener() {
-    const users = JSON.parse(this.responseText);
-    console.log(users);
+﻿//function regListener() {
+//    const users = JSON.parse(this.responseText);
+//    console.log(users);
+//}
+
+//const xhr = new XMLHttpRequest();
+//xhr.addEventListener('load', regListener);
+//xhr.open('GET', '/Data/GetSchedules');
+//xhr.send();
+
+
+$(document).ready(function () {
+    
+
+});
+
+function getUsers() {
+    var uri = 'Data/GetUsers';
+
+    $.getJSON(uri)
+        .done(function (data) {
+            //$("#products").empty();
+            // On success, 'data' contains a list of products.
+            $.each(data, function (key, item) {
+                // Add a list item for the product.
+                $('<li>', { text: formatItem(item) }).appendTo($('#products'));
+            });
+        });
 }
 
-const xhr = new XMLHttpRequest();
-xhr.addEventListener('load', regListener);
-xhr.open('GET', '/Data/GetSchedules');
-xhr.send();
+function formatItem(item) {
+    return item.name + ' email ' + item.email;
+}
