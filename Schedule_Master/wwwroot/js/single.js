cdda7b7085952a0c5a9b30a96ce573feb71cd973
kAndris2 <k.andris258@gmail.com>
2020-05-12 10:33:23 +0200

    js

--- Schedule_Master/wwwroot/js/single.js
+++ Schedule_Master/wwwroot/js/single.js
@@ -1,9 +1,25 @@
-﻿function regListener() {
+﻿function onLoadUsers() {
     const users = JSON.parse(this.responseText);
     console.log(users);
 }
 
-const xhr = new XMLHttpRequest();
-xhr.addEventListener('load', regListener);
-xhr.open('GET', '/Data/GetSchedules');
-xhr.send();
+function onLoad() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', onLoadUsers);
+    xhr.open('GET', '/Data/GetUsers');
+    xhr.send();
+    //
+    xhr.addEventListener('load', onLoadSchedules);
+    xhr.open('GET', '/Data/GetSchedules');
+    xhr.send();
+}
+
+function User(id, name, email, password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+}
+
+const user;
