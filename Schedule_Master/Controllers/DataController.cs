4401f37cf9b567b5f7331f5209676f330f893c8c
kAndris2 <k.andris258@gmail.com>
2020-05-14 15:36:49 +0200

    back

--- Schedule_Master/Controllers/DataController.cs
+++ Schedule_Master/Controllers/DataController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -17,8 +18,9 @@ namespace Schedule_Master.Controllers
         public List<UserModel> GetUsers() { return Singleton.Users; }
 
         [HttpGet("GetSchedules")]
-        public List<ScheduleModel> GetSchedules() { return Singleton.Schedules; }
+        public List<ScheduleModel> GetSchedules() { return Singleton.GetSchedules(); }
 
+        /*
         [HttpGet("GetColumns")]
         public List<ColumnModel> GetColumns() { return Singleton.Columns; }
 
@@ -27,11 +29,17 @@ namespace Schedule_Master.Controllers
 
         [HttpGet("GetTasks")]
         public List<TaskModel> GetTasks() { return Singleton.Tasks; }
+        */
 
         [HttpPost("Schedule")]
         public void Schedule(string[] table)
         {
-            Console.WriteLine(table[0].Length);
+            DateTime start = DateTime.ParseExact(table[1], "ddd MMM dd yyyy HH:mm:ss 'GMT'K", CultureInfo.InvariantCulture);
+            DateTime end = DateTime.ParseExact(table[2], "ddd MMM dd yyyy HH:mm:ss 'GMT'K", CultureInfo.InvariantCulture);
+            long start_m = (long)(start - new DateTime(1970, 1, 1)).TotalMilliseconds;
+            long end_m = (long)(end - new DateTime(1970, 1, 1)).TotalMilliseconds;
+            //
+            Singleton.CreateSchedule(table[0], start_m, end_m, int.Parse(table[3]), table[4] == "true");
         }
     }
 }
--- Schedule_Master/Models/ScheduleModel.cs
+++ Schedule_Master/Models/ScheduleModel.cs
@@ -8,16 +8,23 @@ namespace Schedule_Master.Models
     public class ScheduleModel
     {
         public int ID { get; }
-        public String Title { get; private set; }
+        public String Title { get; }
+        //public String Url { get; }
         public int User_ID { get; }
+        public long Start { get; }
+        public long End { get; }
+        public bool Allday { get; }
 
-        public List<ColumnModel> Columns = new List<ColumnModel>();
+        //public List<ColumnModel> Columns = new List<ColumnModel>();
 
-        public ScheduleModel(int id, string title, int userid)
+        public ScheduleModel(int id, string title, int userid, long start, long end, bool allday)
         {
             ID = id;
             Title = title;
             User_ID = userid;
+            Start = start;
+            End = end;
+            Allday = allday;
         }
     }
 }
